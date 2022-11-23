091119bbe8e107b0a027e11055f29f86b1708b1f
Huu Tu <huutu7165@gmail.com>
2022-11-23 13:40:38 +0700

    Profile admin

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -82,7 +82,7 @@ export default function Header(){
                   <BsPersonCircle />
                 </a>
                 <div className="dropdown-menu">
-                  <a className="dropdown-item" href="/profile">{profile.fullName}</a>
+                  <a className="dropdown-item" href={`/profileAdmin/${profile._id}`}>{profile.fullName}</a>
                   <a className="dropdown-item" href="/discussion">Discussions</a>
                   <div className="dropdown-divider"></div>
                   <a className="dropdown-item" href="/" onClick={handleLogOut}>Log out</a>
--- frontend/src/modules/Admin/Pages/headerAdmin.jsx
+++ frontend/src/modules/Admin/Pages/headerAdmin.jsx
@@ -26,6 +26,10 @@ export default function HeaderAdmin(){
     getNotify()
   },[])
 
+  useEffect(()=>{
+
+  })
+
   return(
     <>
       {/* Navigation  */}
@@ -62,7 +66,7 @@ export default function HeaderAdmin(){
                   <BsPersonCircle />
                 </a>
                 <div className="dropdown-menu">
-                  <a className="dropdown-item" href="/profile">{profile.fullName}</a>
+                  <a className="dropdown-item" href={`/profile/${profile._id}`}>{profile.fullName}</a>
                   <a className="dropdown-item" href="/discussion">Discussions</a>
                   <div className="dropdown-divider"></div>
                   <a className="dropdown-item" href="/" onClick={handleLogOut}>Log out</a>
